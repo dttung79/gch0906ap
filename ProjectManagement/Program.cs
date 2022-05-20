180d57ab003fac7873a68d162fed419412a804f7
dttung79 <dttung@gmail.com>
2022-05-20 15:35:40 +0700

    update Project class

--- ProjectManagement/Program.cs
+++ ProjectManagement/Program.cs
@@ -1,36 +1,55 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace ProjectManagement
 {
     class Program
     {
-        static void Main(string[] args)
+        internal static void Main(string[] args)
         {
-            // create 4 members
-            // Member john = new Member("John Lennon", 5, "Developer");
-            // Member paul = new Member("Paul McCartney", 3, "Test Engineer");
-            // Member george = new Member("George Harrison", 4, "Designer");
-            // Member ringo = new Member("Ringo Starr", 2, "Team Leader");
-            // // create 1 project
-            // Project project = new Project("Project Axie", 1000000);
-            // // add 4 members to project
-            // project.AddMember(john);
-            // project.AddMember(paul);
-            // project.AddMember(george);
-            // project.AddMember(ringo);
-            // // show project details
-            // project.Show();
-
-            // Homework
             // Ask how many projects will be created
-            // For each project
+            Console.WriteLine("How many projects will be created?");
+            int nProjects = int.Parse(Console.ReadLine());
+            List<Project> projects = new List<Project>();
+            for (int i = 0; i < nProjects; i++)
+            {
+                Project p = CreateNewProject();
+                projects.Add(p);
+            }
+            foreach (Project p in projects)
+            {
+                p.Show();
+                System.Console.WriteLine("------------------------");
+            }
+        }
+
+        private static Project CreateNewProject()
+        {
             //      Ask for project name, budget
-            //      Ask how many members will be added to the project
-            //      For each member, ask for name, years, role
-            //      Add member to project
-            //      Show project details
-            // Note: Use functions so that number of lines does not exeed 25
-            // for each function
+            Console.WriteLine("Enter project name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter project budget:");
+            double budget = Double.Parse(Console.ReadLine());
+            Project p = new Project(name, budget);
+            //     Ask for number of members
+            Console.WriteLine("How many members in this project?");
+            int nMembers = int.Parse(Console.ReadLine());
+            for (int i = 0; i < nMembers; i++)
+            {
+                Member m = CreateNewMember();
+                p.AddMember(m);
+            }
+            return p;
+        }
+
+        private static Member CreateNewMember()
+        {
+            //      Ask for member name, experience, role
+            Console.WriteLine("Enter member name:");        string name     = Console.ReadLine();
+            Console.WriteLine("Enter member experience:");  int experience  = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter member role:");        string role     = Console.ReadLine();
+            
+            Member m = new Member(name, experience, role);
+            return m;
         }
     }
 }
